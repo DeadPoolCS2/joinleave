5c454685812d8156b9d0b0e3cb310109296e1697
Andrei Sebastian <andreieurt@gmail.com>
2024-07-11 20:17:44 +0300

    upload(src): First Upload

---
+++ README.md
@@ -0,0 +1,44 @@
+<p align="center">
+  <a href="https://github.com/swiftly-solution/joinleave">
+    <img src="https://cdn.swiftlycs2.net/swiftly-logo.png" alt="SwiftlyLogo" width="80" height="80">
+  </a>
+
+  <h3 align="center">[Swiftly] Join/Leave messages</h3>
+
+  <p align="center">
+    A plugin for Swiftly that prints a message when a player connects on/disconnects from the server.
+    <br/>
+  </p>
+</p>
+
+<p align="center">
+  <img src="https://img.shields.io/github/downloads/swiftly-solution/joinleave/total" alt="Downloads"> 
+  <img src="https://img.shields.io/github/contributors/swiftly-solution/joinleave?color=dark-green" alt="Contributors">
+  <img src="https://img.shields.io/github/issues/swiftly-solution/joinleave" alt="Issues">
+  <img src="https://img.shields.io/github/license/swiftly-solution/joinleave" alt="License">
+</p>
+
+---
+
+### Installation 👀
+
+1. Download the newest [release](https://github.com/swiftly-solution/joinleave/releases).
+2. Everything is drag & drop, so I think you can do it!
+
+### Configuring the plugin 🧐
+
+* After installing the plugin, you can change the messages from `addons/swiftly/translations`.
+* After installing the plugin, you can change the configuration from `addons/swiftly/configs/plugins`.
+
+### Creating A Pull Request 😃
+
+1. Fork the Project
+2. Create your Feature Branch
+3. Commit your Changes
+4. Push to the Branch
+5. Open a Pull Request
+
+### Have ideas/Found bugs? 💡
+Join [Swiftly Discord Server](https://swiftlycs2.net/discord) and send a message in the topic from `📕╎plugins-sharing` of this plugin!
+
+---
---
+++ plugins/joinleave/main.lua
@@ -0,0 +1,60 @@
+AddEventHandler("OnClientDisconnect", function(event, playerid)
+	local player = GetPlayer(playerid)
+    if not player then return EventResult.Continue end
+    if player:IsFakeClient() then return EventResult.Continue end
+        
+    local name = player:CBasePlayerController().PlayerName
+    local ip = player:GetIPAddress()
+        
+    PerformHTTPRequest("http://ip-api.com/json/"..ip, function(status, body, headers, err)
+       	local requestData = json.decode(body)
+		if not requestData then return end
+		if config:Fetch("joinleave.log") then logger:Write(LogType_t.Common, FetchTranslation("joinleave.leave.log"):gsub("{NAME}", name):gsub("{IP}", ip):gsub("{CITY}", requestData.city):gsub("{COUNTRY}", requestData.country)) end
+		if config:Fetch("joinleave.console") then 
+			local res, _ = FetchTranslation("joinleave.leave.console"):gsub("{NAME}", name):gsub("{IP}", ip):gsub("{CITY}", requestData.city):gsub("{COUNTRY}", requestData.country)
+			print(res)
+		end
+		playermanager:SendMsg(MessageType.Chat, FetchTranslation("joinleave.leave.game"):gsub("{PREFIX}", config:Fetch("joinleave.prefix")):gsub("{NAME}", name):gsub("{IP}", ip):gsub("{CITY}", requestData.city):gsub("{COUNTRY}", requestData.country))
+    end)
+        
+	return EventResult.Continue
+end)
+
+AddEventHandler("OnPlayerConnectFull", function(event)
+    local playerid = event:GetInt("userid")
+	local player = GetPlayer(playerid)
+    if not player then return EventResult.Continue end
+    if player:IsFakeClient() then return EventResult.Continue end
+        
+    local name = player:CBasePlayerController().PlayerName
+    local ip = player:GetIPAddress()
+        
+    PerformHTTPRequest("http://ip-api.com/json/"..ip, function(status, body, headers, err)
+       	local requestData = json.decode(body)
+		if not requestData then return end
+		if config:Fetch("joinleave.log") then logger:Write(LogType_t.Common, FetchTranslation("joinleave.join.log"):gsub("{NAME}", name):gsub("{IP}", ip):gsub("{CITY}", requestData.city):gsub("{COUNTRY}", requestData.country)) end
+		if config:Fetch("joinleave.console") then 
+			local res, _  = FetchTranslation("joinleave.join.console"):gsub("{NAME}", name):gsub("{IP}", ip):gsub("{CITY}", requestData.city):gsub("{COUNTRY}", requestData.country)
+			print(res)
+        end
+		playermanager:SendMsg(MessageType.Chat, FetchTranslation("joinleave.join.game"):gsub("{PREFIX}", config:Fetch("joinleave.prefix")):gsub("{NAME}", name):gsub("{IP}", ip):gsub("{CITY}", requestData.city):gsub("{COUNTRY}", requestData.country))
+    end)
+        
+	return EventResult.Continue
+end)
+
+function GetPluginAuthor()
+    return "Swiftly Solutions"
+end
+
+function GetPluginVersion()
+    return "1.0.0"
+end
+
+function GetPluginName()
+    return "Join Leave Messages"
+end
+
+function GetPluginWebsite()
+    return "https://github.com/swiftly-solution/joinleave"
+end
